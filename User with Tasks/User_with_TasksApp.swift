b1c40b254fa054c3918a111fd6777eb57101f7b0
Immortal215 <sharulshah@icloud.com>
2024-10-10 08:44:02 -0500

    Initial Commit

---
+++ User with Tasks/User_with_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_with_TasksApp.swift
+//  User with Tasks
+//
+//  Created by Sharul M. Shah on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct User_with_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
